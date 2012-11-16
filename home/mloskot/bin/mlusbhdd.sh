c113a8ac42802acd971135de46c90ad32c16ac19
Mateusz Loskot <mateusz@loskot.net>
2012-11-16 21:46:14 +0000

    Made mount and umount commants short: m and u respectively

--- home/mloskot/bin/mlusbhdd.sh
+++ home/mloskot/bin/mlusbhdd.sh
@@ -2,26 +2,25 @@
 #
 # usbhdd.sh - mount or umount external USB hard drive
 #
-CMD=$1
 DEV=$2
 MNT=$3
 function usage() {
     s=`basename $0`
-    echo "Usage: ${s} <mount|umount> <device> <mount point>"
+    echo "Usage: ${s} <m|u> <device> <mount point>"
     exit 1
 }
 function mount() {
-    echo "$CMD -o uid=mloskot,gid=users,fmask=113,dmask=002 $DEV $MNT"
-    sudo $CMD -o uid=mloskot,gid=users,fmask=113,dmask=002 $DEV $MNT
+    echo "mount -o uid=mloskot,gid=users,fmask=113,dmask=002 $DEV $MNT"
+    sudo mount -o uid=mloskot,gid=users,fmask=113,dmask=002 $DEV $MNT
 }
 function umount() {
-    echo "$CMD $DEV"
-    sudo $CMD $DEV
+    echo "umount $DEV"
+    sudo umount $DEV
 }
 [[ $# -eq 0 ]] && usage
-if [ "$1" = "mount" ]; then
+if [ "$1" = "m" ]; then
     mount
-elif [ "$1"="umount" ]; then
+elif [ "$1"="u" ]; then
     umount
 else
     usage
