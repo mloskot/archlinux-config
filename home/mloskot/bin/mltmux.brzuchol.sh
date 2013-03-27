efaed9af211605b71db6bb27d75a0e683146b21c
Mateusz Loskot <mloskot@brzuchol.loskot.net>
2013-03-28 03:01:34 +0400

    Rearange tmux windows

--- home/mloskot/bin/mltmux.brzuchol.sh
+++ home/mloskot/bin/mltmux.brzuchol.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 tmux new-session -d -s 0
-tmux new-window -t 0:0 -n 'general'
-tmux new-window -t 0:1 -n 'root'
-tmux new-window -t 0:2 -n 'oracle'
-tmux new-window -t 0:3 -n 'db2'
+tmux new-window -t 0:0 -n 'root'
+tmux new-window -t 0:1 -n 'home'
+tmux new-window -t 0:2 -n 'code'
+tmux new-window -t 0:3 -n 'oracle'
+tmux new-window -t 0:4 -n 'db2'
 tmux select-window -t 0:0
 tmux -2 attach-session -t 0
