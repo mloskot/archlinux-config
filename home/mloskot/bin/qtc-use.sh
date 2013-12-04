53b2844af2de7a4faca66d4a7951e7cec0c0d5f7
Mateusz Loskot <mateusz@loskot.net>
2013-12-04 20:30:01 +0000

    Comments on Qt Creator launchers

--- home/mloskot/bin/qtc-use.sh
+++ home/mloskot/bin/qtc-use.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# Launches Qt Creator for regular use as IDE
 XQTC=$HOME/dev/qt/_git/qt-creator/_build.use
 ${XQTC}/bin/qtcreator -style gtk
--- home/mloskot/bin/qtc.sh
+++ home/mloskot/bin/qtc.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+# Launches Qt Creator for plugins testing and debugging.
+# This executable and arguments are also used in the Run Configuration
+# of a plugin project.
 XQTC=$HOME/dev/qt/_git/qt-creator/_build
 ${XQTC}/bin/qtcreator -style gtk -color red -settingspath ${XQTC}/.config
