d299047daa982e4acc05afb8fe485d723869db10
Mateusz Loskot <mateusz@loskot.net>
2013-12-05 11:22:09 +0000

    Log build start/finish times

--- home/mloskot/bin/build-qt.sh
+++ home/mloskot/bin/build-qt.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-./configure -developer-build -opensource -nomake examples -nomake tests \
-    -confirm-license && \
+date > LOG
+./configure -developer-build -opensource -nomake examples -nomake tests -confirm-license && \
 make -j 4
+date >> LOG
