d18f69af61131bf1049fa8f06a84b4d401f66ba9
Mateusz Loskot <mateusz@loskot.net>
2015-04-13 22:14:02 +0200

    Add volumeicon for VirtualBox

---
+++ home/mloskot/.config/volumeicon/volumeicon.vb
@@ -0,0 +1,26 @@
+[Alsa]
+card=Intel 82801AA-ICH
+channel=Master
+
+[Notification]
+show_notification=false
+notification_type=0
+
+[StatusIcon]
+stepsize=5
+onclick=xterm -e 'alsamixer'
+theme=Blue Bar
+use_panel_specific_icons=false
+lmb_slider=true
+mmb_mute=true
+use_horizontal_slider=false
+show_sound_level=true
+use_transparent_background=false
+
+[Hotkeys]
+up_enabled=true
+down_enabled=true
+mute_enabled=true
+up=XF86AudioRaiseVolume
+down=XF86AudioLowerVolume
+mute=XF86AudioMute
